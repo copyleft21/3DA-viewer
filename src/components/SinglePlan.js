846e8f7167cad172af67995872b495cc140904f5
Moayyad-Zakkar <Moayyad-Zakkar@github.com>
2021-02-28 23:48:38 +0200

    fetch front

--- src/components/SinglePlan.js
+++ src/components/SinglePlan.js
@@ -16,6 +16,7 @@ function SinglePlan() {
           `*[slug.current == $slug]{
             caseNum,
             patientName,
+            front,
             slug
          }`,
           { slug }
