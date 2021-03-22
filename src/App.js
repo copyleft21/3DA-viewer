4d3ea51e5274d68b25f095995364ec02554a7f6f
Moayyad-Zakkar <Moayyad-Zakkar@github.com>
2021-03-23 01:56:37 +0200

    Revert "added /plans route"
    This reverts commit b2245a9f107dd8d61babcd898eb39188e12404e7.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SinglePlan from "./components/SinglePlan";
 function App() {
   return (
     <BrowserRouter>
-        <Route component={SinglePlan} path="/plans/:slug" />
+        <Route component={SinglePlan} path="/:slug" />
     </BrowserRouter>
   );
 }
