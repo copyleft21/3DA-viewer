9833b5a980c143bb715dfd27b4d720f4a23cea9c
Moayyad-Zakkar <Moayyad-Zakkar@github.com>
2021-03-24 00:07:24 +0200

    Added Soon page on no-slug path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import SinglePlan from "./components/SinglePlan";
+import Soon from "./components/Soon";
 
 function App() {
   return (
     <BrowserRouter>
         <Route component={SinglePlan} path="/:slug" />
+        <Route component={Soon} path="/" exact />
     </BrowserRouter>
   );
 }
