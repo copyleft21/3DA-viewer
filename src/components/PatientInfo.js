36a5a9d53fddb7a63839714867c45386d612c571
Moayyad-Zakkar <Moayyad-Zakkar@github.com>
2021-04-17 15:29:13 +0300

    Added Doctor Name
    prettier activated too;

--- src/components/PatientInfo.js
+++ src/components/PatientInfo.js
@@ -1,68 +1,79 @@
-import React from 'react'
-import styled from 'styled-components'
-import sheetIcon from '../assets/imgs/sheet.svg'
+import React from "react";
+import styled from "styled-components";
+import sheetIcon from "../assets/imgs/sheet.svg";
 
+const PatientInfo = ({ name, caseNum, patientSheet, doctorName }) => {
+  return (
+    <Flex>
+      <PatientInfoText>
+        Patient Name: <PatientInfoTextSpan>{name}</PatientInfoTextSpan>
+      </PatientInfoText>
+      {doctorName ? (
+        <PatientInfoText>
+          Doctor Name: <PatientInfoTextSpan>{doctorName}</PatientInfoTextSpan>
+        </PatientInfoText>
+      ) : null}
 
-
-const PatientInfo = ({name, caseNum, patientSheet}) => {
-    return (
-        <Flex>
-         <PatientInfoText>Patient Name: <PatientInfoTextSpan>{name}</PatientInfoTextSpan></PatientInfoText>   
-         <PatientInfoText>Case Number: <PatientInfoTextSpan>{caseNum}</PatientInfoTextSpan></PatientInfoText>    
-         {patientSheet ?
-                  <FlexRow href="/information sheet.pdf">
-                  <ViewIcon src={sheetIcon} /> 
-                  <PatientInfoText>Download <PatientInfoTextSpan>Information Sheet</PatientInfoTextSpan> (Arabic)</PatientInfoText>
-                  </FlexRow>
-                  : null
-        }
-
-        </Flex>
-    )
-}
+      <PatientInfoText>
+        Case Number: <PatientInfoTextSpan>{caseNum}</PatientInfoTextSpan>
+      </PatientInfoText>
+      {patientSheet ? (
+        <FlexRow href="/information sheet.pdf">
+          <ViewIcon src={sheetIcon} />
+          <PatientInfoText>
+            Download{" "}
+            <PatientInfoTextSpan>Information Sheet</PatientInfoTextSpan>{" "}
+            (Arabic)
+          </PatientInfoText>
+        </FlexRow>
+      ) : null}
+    </Flex>
+  );
+};
 
 const Flex = styled.div`
-display:flex;
-flex-direction: column;
-text-align: center; 
-align-items: center;
-justify-content: center;
-margin: 0 20px;
-`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  margin: 0 20px;
+`;
 const FlexRow = styled.a`
-display:flex;
-flex-direction: Row;
-cursor: pointer;
-text-align: center; 
-align-items: center;
-justify-content: center;
-filter: invert(47%) sepia(72%) saturate(2534%) hue-rotate(165deg) brightness(102%) contrast(101%);
-/* border: 1px solid #fff; */
-border-radius: 5px;
-padding: 0 5px;
-margin: 20px 0 0px 0;
-text-decoration: none;
-`
+  display: flex;
+  flex-direction: Row;
+  cursor: pointer;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  filter: invert(47%) sepia(72%) saturate(2534%) hue-rotate(165deg)
+    brightness(102%) contrast(101%);
+  /* border: 1px solid #fff; */
+  border-radius: 5px;
+  padding: 0 5px;
+  margin: 20px 0 0px 0;
+  text-decoration: none;
+`;
 
 const PatientInfoText = styled.p`
-    /* color: #00adef;
+  /* color: #00adef;
     font-weight: bold; */
-    margin: 0;
-    font-size: 1rem;
-`
+  margin: 0;
+  font-size: 1rem;
+`;
 
 const PatientInfoTextSpan = styled.span`
-    color: #00adef;
-    font-weight: bold;
-    margin: 0;
-    font-size: 1rem;
-`
+  color: #00adef;
+  font-weight: bold;
+  margin: 0;
+  font-size: 1rem;
+`;
 
 const ViewIcon = styled.img`
-    width: 43px;
-    height: 40px;
-    display: block;
-    margin: 10px 0;
-`
+  width: 43px;
+  height: 40px;
+  display: block;
+  margin: 10px 0;
+`;
 
 export default PatientInfo;
--- src/components/SinglePlan.js
+++ src/components/SinglePlan.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import styled from 'styled-components'
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import sanityClient from "../client.js";
-import Container from './Container';
-import ReactPlayer from 'react-player'
-import JawViewBox from './JawViewsBox.js';
-import Logo from './Logo';
-import Views from './Views.js';
-import PlayerButtons from './PlayerButtons.js';
-import PatientInfo from './PatientInfo.js';
-import Tabs from './Tabs.js';
-
+import Container from "./Container";
+import ReactPlayer from "react-player";
+import JawViewBox from "./JawViewsBox.js";
+import Logo from "./Logo";
+import Views from "./Views.js";
+import PlayerButtons from "./PlayerButtons.js";
+import PatientInfo from "./PatientInfo.js";
+import Tabs from "./Tabs.js";
 
 function SinglePlan() {
-    const [planData, setplanData] = useState(null);
-    const { slug } = useParams();
-    const [view, setView] = useState('Front');
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentTab, setCurrentTab] = useState('videos')
-    const player = useRef();
-    useEffect(() => {
-      sanityClient
-        .fetch(
-          `*[slug.current == $slug]{
+  const [planData, setplanData] = useState(null);
+  const { slug } = useParams();
+  const [view, setView] = useState("Front");
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTab, setCurrentTab] = useState("videos");
+  const player = useRef();
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == $slug]{
             caseNum,
             patientName,
+            doctorName,
             patientSheet,
             upperCount,
             lowerCount,
@@ -68,179 +68,223 @@ function SinglePlan() {
             },
                         slug
                      }`,
-          { slug }
-        )
-        .then((data) => setplanData(data[0]))
-        .catch(console.error);
+        { slug }
+      )
+      .then((data) => setplanData(data[0]))
+      .catch(console.error);
+  }, [slug]);
 
-    }, [slug]);
-  
-    if (!planData) return <div>Loading...</div>;
-    
-    // Change the Tab function
+  if (!planData) return <div>Loading...</div>;
 
-    const tabChanger = (tab) => {
-      setCurrentTab(tab)
-    }
+  // Change the Tab function
 
-    // Change the View function
-    const viewChanger = (viewName) => {
-      setView(viewName)
-    }
+  const tabChanger = (tab) => {
+    setCurrentTab(tab);
+  };
 
-    // Aligners Count
-    const upperAligners = planData.upperCount;
-    const lowerAligners = planData.lowerCount;
+  // Change the View function
+  const viewChanger = (viewName) => {
+    setView(viewName);
+  };
 
-    // Patients Information
-    const patientName = planData.patientName;
-    const caseNum = planData.caseNum;
-    const patientSheet = planData.patientSheet;
+  // Aligners Count
+  const upperAligners = planData.upperCount;
+  const lowerAligners = planData.lowerCount;
 
-    // Logo
-    const logoLink = planData.logo?.asset.url;
-    const estimatedTime = planData.estimatedTime;
+  // Patients Information
+  const patientName = planData.patientName;
+  let doctorName;
+  if (planData.doctorName) {
+    doctorName = planData.doctorName;
+  }
 
+  const caseNum = planData.caseNum;
+  const patientSheet = planData.patientSheet;
 
-    // Video Existence Checking Or Image Existence
-    let lowerView , upperView , rightView , leftView
+  // Logo
+  const logoLink = planData.logo?.asset.url;
+  const estimatedTime = planData.estimatedTime;
 
-    if (planData.sequence.lower?.asset.url || planData.beforeAfter.lower?.asset.url ) {
-       lowerView = true;
-    } else {
-       lowerView = false
-    }
-    if (planData.sequence.upper?.asset.url || planData.beforeAfter.upper?.asset.url ) {
-      upperView = true;
-    } else {
-      upperView = false;
-    }
-    if (planData.sequence.right?.asset.url || planData.beforeAfter.right?.asset.url) {
-      rightView = true;
-    } else {
-      rightView = false;
-    }
-    if (planData.sequence.left?.asset.url || planData.beforeAfter.left?.asset.url) {
-      leftView = true;
-    } else {
-      leftView = false;
-    }
+  // Video Existence Checking Or Image Existence
+  let lowerView, upperView, rightView, leftView;
 
-    let videoSrc, imgSrc;
-    
-    if (view === 'Front') {
-      videoSrc = planData.sequence.front.asset.url;
-      imgSrc = planData.beforeAfter?.front.asset.url;
+  if (
+    planData.sequence.lower?.asset.url ||
+    planData.beforeAfter.lower?.asset.url
+  ) {
+    lowerView = true;
+  } else {
+    lowerView = false;
+  }
+  if (
+    planData.sequence.upper?.asset.url ||
+    planData.beforeAfter.upper?.asset.url
+  ) {
+    upperView = true;
+  } else {
+    upperView = false;
+  }
+  if (
+    planData.sequence.right?.asset.url ||
+    planData.beforeAfter.right?.asset.url
+  ) {
+    rightView = true;
+  } else {
+    rightView = false;
+  }
+  if (
+    planData.sequence.left?.asset.url ||
+    planData.beforeAfter.left?.asset.url
+  ) {
+    leftView = true;
+  } else {
+    leftView = false;
+  }
 
-    } else if (view === 'Right') {
-      videoSrc = planData.sequence.right.asset.url;
-      imgSrc = planData.beforeAfter?.right.asset.url;
-    } else if (view === 'Left') {
-      videoSrc = planData.sequence.left.asset.url;
-      imgSrc = planData.beforeAfter?.left.asset.url;
-    } else if (view === 'Upper') {
-      videoSrc = planData.sequence.upper.asset.url;
-      imgSrc = planData.beforeAfter?.upper.asset.url;
-    } else if (view === 'Lower') {
-      videoSrc = planData.sequence.lower.asset.url;
-      imgSrc = planData.beforeAfter?.lower.asset.url;
-    }
+  let videoSrc, imgSrc;
 
-    // Video player Buttons functions
-    const togglePlay = () => {
-      setIsPlaying(!isPlaying)
-    }
+  if (view === "Front") {
+    videoSrc = planData.sequence.front.asset.url;
+    imgSrc = planData.beforeAfter?.front.asset.url;
+  } else if (view === "Right") {
+    videoSrc = planData.sequence.right.asset.url;
+    imgSrc = planData.beforeAfter?.right.asset.url;
+  } else if (view === "Left") {
+    videoSrc = planData.sequence.left.asset.url;
+    imgSrc = planData.beforeAfter?.left.asset.url;
+  } else if (view === "Upper") {
+    videoSrc = planData.sequence.upper.asset.url;
+    imgSrc = planData.beforeAfter?.upper.asset.url;
+  } else if (view === "Lower") {
+    videoSrc = planData.sequence.lower.asset.url;
+    imgSrc = planData.beforeAfter?.lower.asset.url;
+  }
 
-    const seekToStart = () => {
-      setIsPlaying(false)
-      player.current.seekTo(0);
-    }
-    const seekToEnd = async () => {
-      await player.current.seekTo(.99, 'fraction');
-      // setIsPlaying(false)
-    }
+  // Video player Buttons functions
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
 
-    const seekOneSecForward = async () => {
-      const currentTime = player.current.getCurrentTime()
-      await player.current.seekTo(currentTime + 1 , 'seconds');
-      // setIsPlaying(false)
-    }
+  const seekToStart = () => {
+    setIsPlaying(false);
+    player.current.seekTo(0);
+  };
+  const seekToEnd = async () => {
+    await player.current.seekTo(0.99, "fraction");
+    // setIsPlaying(false)
+  };
 
-    const seekOneSecBackward = () => {
-      setIsPlaying(false)
-      const currentTime = player.current.getCurrentTime()
-      player.current.seekTo(currentTime - 1, 'seconds');
-    } 
+  const seekOneSecForward = async () => {
+    const currentTime = player.current.getCurrentTime();
+    await player.current.seekTo(currentTime + 1, "seconds");
+    // setIsPlaying(false)
+  };
 
-    // JSX !!!
+  const seekOneSecBackward = () => {
+    setIsPlaying(false);
+    const currentTime = player.current.getCurrentTime();
+    player.current.seekTo(currentTime - 1, "seconds");
+  };
 
-    return (
+  // JSX !!!
+
+  return (
     <Content>
       <Logo logo={logoLink} />
       <Container>
-      <PatientInfo name={patientName} caseNum={caseNum}  patientSheet={patientSheet} />
-      <Tabs currentTab={currentTab} tabChanger={tabChanger} />
-      {currentTab === 'videos' ? 
-      <>
-            <Views upperView={upperView} lowerView={lowerView} leftView={leftView} rightView={rightView} currentView={view} viewChanger={viewChanger} />
+        <PatientInfo
+          name={patientName}
+          caseNum={caseNum}
+          patientSheet={patientSheet}
+          doctorName={doctorName}
+        />
+        <Tabs currentTab={currentTab} tabChanger={tabChanger} />
+        {currentTab === "videos" ? (
+          <>
+            <Views
+              upperView={upperView}
+              lowerView={lowerView}
+              leftView={leftView}
+              rightView={rightView}
+              currentView={view}
+              viewChanger={viewChanger}
+            />
             <DataContainer>
               <ReactPlayer
-               ref={player}
-               playing={isPlaying} 
-               controls={false} 
-               onEnded={() => setIsPlaying(false)} 
-               url={videoSrc} 
-               width="100%" height="100%"
-               loop={false}
-                />
+                ref={player}
+                playing={isPlaying}
+                controls={false}
+                onEnded={() => setIsPlaying(false)}
+                url={videoSrc}
+                width="100%"
+                height="100%"
+                loop={false}
+              />
             </DataContainer>
-      
-            <PlayerButtons 
-            onPlayClick={togglePlay}
-            playing={isPlaying}
-            seekToStart={seekToStart}
-            seekToEnd={seekToEnd}
-            seekOneSecForward={seekOneSecForward}
-            seekOneSecBackward={seekOneSecBackward}
+
+            <PlayerButtons
+              onPlayClick={togglePlay}
+              playing={isPlaying}
+              seekToStart={seekToStart}
+              seekToEnd={seekToEnd}
+              seekOneSecForward={seekOneSecForward}
+              seekOneSecBackward={seekOneSecBackward}
             />
-            </>
-      : 
-      <>
-      <Views upperView={upperView} lowerView={lowerView} leftView={leftView} rightView={rightView} currentView={view} viewChanger={viewChanger} />
-      <DataContainer>
-      <DataImage src={imgSrc} alt=''/>
-      </DataContainer>
-      </>
-      }
-      {estimatedTime ? <Estimated>Estimated treatment time: <EstimatedSpan>{estimatedTime}</EstimatedSpan> months</Estimated> : null}
-      <JawViewBox upper={upperAligners} lower={lowerAligners} />
+          </>
+        ) : (
+          <>
+            <Views
+              upperView={upperView}
+              lowerView={lowerView}
+              leftView={leftView}
+              rightView={rightView}
+              currentView={view}
+              viewChanger={viewChanger}
+            />
+            <DataContainer>
+              <DataImage src={imgSrc} alt="" />
+            </DataContainer>
+          </>
+        )}
+        {estimatedTime ? (
+          <Estimated>
+            Estimated treatment time:{" "}
+            <EstimatedSpan>{estimatedTime}</EstimatedSpan> months
+          </Estimated>
+        ) : null}
+        <JawViewBox upper={upperAligners} lower={lowerAligners} />
       </Container>
-      <CopyRightText>3DA © 2021 - Designed by: <FooterLink href="https://github.com/Moayyad-Zakkar">Moayyad Zakkar</FooterLink></CopyRightText>
+      <CopyRightText>
+        3DA © 2021 - Designed by:{" "}
+        <FooterLink href="https://github.com/Moayyad-Zakkar">
+          Moayyad Zakkar
+        </FooterLink>
+      </CopyRightText>
     </Content>
-    );
+  );
 }
 
 const FooterLink = styled.a`
-/* text-decoration: none; */
-`
+  /* text-decoration: none; */
+`;
 
 const DataContainer = styled.div`
-height: 400px;
-width: 90%;
-display: flex; 
-flex-direction: column;
-align-items: center;
-justify-content: center;
-@media (max-width: 768px) {
-        width: 100%;
-        height: auto;
+  height: 400px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: auto;
   }
-`
+`;
 
 const DataImage = styled.img`
-max-height: 100%;
-width: 100%;
-`
+  max-height: 100%;
+  width: 100%;
+`;
 
 const Content = styled.div`
   text-align: center;
@@ -253,28 +297,26 @@ const Content = styled.div`
   //justify-content: flex-start;
   font-size: calc(10px + 2vmin);
   color: black;
-`
+`;
 const CopyRightText = styled.p`
-    /* color: #00adef;
+  /* color: #00adef;
     font-weight: bold; */
-    margin: 1rem;
-    font-size: .8rem;
-`
+  margin: 1rem;
+  font-size: 0.8rem;
+`;
 
 const Estimated = styled.p`
-    /* color: #00adef;
+  /* color: #00adef;
     font-weight: bold; */
-    margin: 0;
-    font-size: 1rem;
-`
+  margin: 0;
+  font-size: 1rem;
+`;
 
 const EstimatedSpan = styled.span`
-    color: #00adef;
-    font-weight: bold;
-    margin: 0;
-    font-size: 1rem;
-`
-
-
+  color: #00adef;
+  font-weight: bold;
+  margin: 0;
+  font-size: 1rem;
+`;
 
 export default SinglePlan;
