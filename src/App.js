2110ee2f5ff518b0ce244afcd8fae156ed04fbd7
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2023-09-09 14:28:20 +0200

    Use hash router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import SinglePlan from "./components/SinglePlan";
 import Soon from "./components/Soon";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Route component={SinglePlan} path="/:slug" />
-        <Route component={Soon} path="/" exact />
-    </BrowserRouter>
+    <HashRouter>
+      <Route component={SinglePlan} path="/:slug" />
+      <Route component={Soon} path="/" exact />
+    </HashRouter>
   );
 }
 
